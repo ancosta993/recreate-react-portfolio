2cc71f9d3288df8f08bd9e6d1e0bfea01d104ad9
Anthony Dcosta <adcosta2000@gmail.com>
2022-08-25 21:11:34 -0400

    added download link to Resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,32 +2,39 @@ import React from 'react';
 
 function Resume(){
    return (
-      <section className = 'resume-container'>
-        <h2>
-          Resume
-        </h2>
+    <>
+    <section className = 'resume-container'>
+      <h2>
+        Resume
+      </h2>
 
-        <h3>Front-end Proficiencies</h3>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>jQuery</li>
-          <li>React</li>
-          <li>Bootstrap</li>
-        </ul>
+      <h3>Front-end Proficiencies</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
 
-        <h3>Back-end Proficiencies</h3>
-        <ul>
-          <li>APIs</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>MySQL, Sequelize</li>
-          <li>MongoDB, mongoose</li>
-          <li>REST</li>
-          <li>GraphQL</li>
-        </ul>
-      </section>
+      <h3>Back-end Proficiencies</h3>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MySQL, Sequelize</li>
+        <li>MongoDB, mongoose</li>
+        <li>REST</li>
+        <li>GraphQL</li>
+      </ul>
+    </section>
+
+    <section class='resume-download'>
+        <a href='../../public/Resume.pdf' download>Download Resume</a>
+    </section>
+    
+  </>
     );
 }
 
